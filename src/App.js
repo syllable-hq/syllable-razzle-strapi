84d8dbdbee92d2e2ee2a42f92bd0ba9accf1dccc
bnalvarado94 <bnalvarado94@gmail.com>
2019-08-01 21:38:27 -0600

    add list

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Home from './Components/Home';
+import ProductList from './Components/Product/ProductList';
 import About from './Components/About';
 import Comp404 from './Components/404';
 import './App.scss';
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={ProductList} />
     <Route exact path='/about' component={About} />
     <Route component={Comp404} />
   </Switch>
